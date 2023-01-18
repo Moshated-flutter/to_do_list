36b026604b26c0e46b3bb71fffbbf17fd4443187
moshated <mobin.cofingfluter@gmail.com>
2023-01-18 17:39:25 +0330

    using bloc

---
+++ lib/domain/auth/value_objects.dart
@@ -0,0 +1,28 @@
+import 'package:dartz/dartz.dart';
+import 'package:to_do_list/domain/core/failure.dart';
+import 'package:to_do_list/domain/core/value_validators.dart';
+import 'package:to_do_list/domain/core/valure_objects.dart';
+
+class EmailAddress extends ValueObjects<String> {
+  final Either<ValueFailure<String>, String> value;
+  factory EmailAddress(String input) {
+    assert(input != null);
+    return EmailAddress._(
+      validateEmailAddress(input),
+    );
+  }
+
+  const EmailAddress._(this.value);
+}
+
+class Password extends ValueObjects<String> {
+  final Either<ValueFailure<String>, String> value;
+  factory Password(String input) {
+    assert(input != null);
+    return Password._(
+      validatePassword(input),
+    );
+  }
+
+  const Password._(this.value);
+}
---
+++ lib/domain/core/valure_objects.dart
@@ -0,0 +1,20 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter/foundation.dart';
+import 'package:to_do_list/domain/core/failure.dart';
+
+@immutable
+abstract class ValueObjects<T> {
+  const ValueObjects();
+  Either<ValueFailure<T>, T> get value;
+  @override
+  String toString() => 'Value($value)';
+
+  @override
+  bool operator ==(Object o) {
+    if (identical(this, o)) return true;
+    return o is ValueObjects && o.value == value;
+  }
+
+  @override
+  int get hashCode => value.hashCode;
+}
